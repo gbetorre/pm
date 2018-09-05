2306c2415930c4789a585b5241c5b3cdd60a9d82
Giovanroberto Torre <giovanroberto.torre@univr.it>
2018-09-05 17:26:33 +0200

    Riorganizzato Model

--- src/it/alma/DBWrapper.java
+++ src/it/alma/DBWrapper.java
@@ -28,35 +28,34 @@
  *   Via Dell'Artigliere, 8
  *   37129 Verona (Italy)
  */
+
 package it.alma;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import java.util.logging.Logger;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.sql.DataSource;
+import javax.swing.JOptionPane;
 
 import it.alma.bean.BeanUtil;
 import it.alma.bean.CourseBean;
 import it.alma.exception.NotFoundException;
 import it.alma.exception.WebStorageException;
 
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Vector;
-import java.util.logging.Logger;
-
-import javax.servlet.ServletException;
-import javax.sql.DataSource;
-import javax.swing.JOptionPane;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
 
 /**
  * 
  * @author trrgnr59
- *
+ * @version 2
  */
 public class DBWrapper implements Query {
     
@@ -78,24 +77,6 @@ public class DBWrapper implements Query {
      * <p>Gestore del pool di connessioni al dbms.</p>
      */
     protected static DataSource manager = null;
-    /** 
-     * <p>Connessione ad Access 2016 via ODBC.</p>
-     * <p>Il nome del DB cui accedere.</p>
-     * <p>Prima di tutto bisogna impostare l'origine dati in windows tra le variabili ODBC.</p>
-     * <tt>
-     * Pannello di controllo -> strumenti di amministrazione -> origine dati (odbc) -> in DSN utente
-     * fare aggiungere e in nome origine dati mettere il nome del DB, es. prova.mdb, poi cliccare su "seleziona" e inserire il path del DB.
-     * </tt>
-     * <p>Senza queste impostazioni non ci si può connettere al db, il programma non trova i driver e lo stesso DB.</p>
-     * <small>Notare che nel caso di applicazioni web a 64bit, non sar&agrave;
-     * possibile accedere ad Access 2016 via OCBC perché non esiste il driver
-     * per questo DBMS a 64 bit ma solo a 32 bit!</small>
-     */
-    static final String URL = "jdbc:odbc:Alma2017";
-    /**
-     * <p>Connessione al database.</p>
-     *
-    Connection con;*/
 
     
     /**
@@ -121,10 +102,52 @@ public class DBWrapper implements Query {
                 throw new WebStorageException(FOR_NAME + "Errore generico nel costruttore: " + e.getMessage());
             }
         }
-        //openConnection();
     }
     
     
+    /**
+     * Restituisce un vettore di CourseBean.
+     * Data creazione 07/01/2016
+     *
+     * @param Lingue lingue
+     * @return Vector DipartimentiBean
+     * @throws WebStorageException
+     */
+    public Vector<CourseBean> getADSemplici()
+                                     throws WebStorageException {
+      ResultSet rs = null;
+      Connection con = null;
+      PreparedStatement pst = null;
+      CourseBean course = null;
+      Vector<CourseBean> lista = new Vector<CourseBean>();
+      try {
+        con = manager.getConnection();
+        pst = con.prepareStatement(AD_SEMPLICI);
+        pst.clearParameters();
+        rs = pst.executeQuery();
+        while (rs.next()) {
+          course = new CourseBean();
+          BeanUtil.populate(course, rs);
+          lista.add(course);
+        }
+        return lista;
+      } catch (SQLException sqle) {
+        throw new WebStorageException(sqle.getMessage());
+      } finally {
+        try {
+          con.close();
+        } catch (NullPointerException npe) {
+            throw new WebStorageException(npe.getMessage());
+        } catch (SQLException sqle) {
+          throw new WebStorageException(sqle.getMessage());
+        }
+      }
+    }
+    
+    
+    /* **************************************************************************
+     * =========================================================================
+     */
     /**
      *  metodo per effettuare qualsiasi query sul DB
      * @param query
@@ -163,55 +186,6 @@ public class DBWrapper implements Query {
     }
     
     
-    /**
-     * Restituisce un vettore di .
-     * Data creazione 07/01/2016
-     *
-     * @param Lingue lingue
-     * @return Vector DipartimentiBean
-     * @throws WebStorageException
-     */
-    public Vector<?> getCourse()
-                        throws WebStorageException {
-      ResultSet rs = null;
-      ResultSet rs2 = null;
-      Connection con = null;
-      PreparedStatement pst = null;
-      CourseBean course = null;
-      Vector lista = new Vector();
-      int idDirettore = -2;
-      int idSegreteria = -2;
-      String nomeDirettore = null;
-      String cognomeDirettore = null;
-      //PersonBean direttore = null;
-      String nomeSegreteria = null;
-      //StrutturaServizioBean segreteria = null;
-      try {
-        con = manager.getConnection();
-        pst = con.prepareStatement(AD_2016);
-        pst.clearParameters();
-        //pst.setString(1, lingue.getPrimaLingua());
-        //pst.setString(2, lingue.getPrimaLingua());
-        //pst.setString(3, lingue.getSecondaLingua());
-        rs = pst.executeQuery();
-        while (rs.next()) {
-          course = new CourseBean();
-          BeanUtil.populate(course, rs);
-          lista.add(course);
-        }
-        return lista;
-      } catch (SQLException e) {
-        throw new WebStorageException(e.getMessage());
-      } finally {
-        try {
-          con.close();
-        } catch (SQLException e1) {
-          throw new WebStorageException(e1.getMessage());
-        }
-      }
-    }
-    
-    
     public Vector<String> getVector(ResultSet rs) {
         Vector<String> result = new Vector<String>();
         try {
@@ -224,88 +198,5 @@ public class DBWrapper implements Query {
         }
         return result;
     }
-    
-    
-    //============================================================================
-    /*
-    public void update(String update){
-        try{
-            Statement stmt = con.createStatement();
-            stmt.executeUpdate(update);
-        } catch(SQLException e){
-            JOptionPane.showMessageDialog(null,"Interrogazione fallita: "+e.getMessage(),"Messaggio:",0,null);
-            closeConnection(new DBWrapper());
-        }
-    }
- 
-    
-    /**
-     * <p>Apre la connessione col DB via ODBC.</p>
-     *
-    public void openConnection() {
-        try {
-            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-        } catch (ClassNotFoundException cnfe) {
-            JOptionPane.showMessageDialog(null, "Driver per database non trovato: "+e.getMessage(),"Messaggio:",0,null);
-        } 
-        try {
-            con = DriverManager.getConnection(url,"admin","pippo");//eventualmente user e password, se il DB è rpotetto da password, admin è di default
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null,"Connessione Fallita: "+e.getMessage(),"Messaggio:",0,null);
-        }
-    }
-    
-    //chiude la connessione col DB
-    public void closeConnection(){
-        try{
-            con.close();
-            System.out.println("connessione chiusa");
-        }catch(SQLException e){
-            JOptionPane.showMessageDialog(null,"Connessione Fallita: "+e.getMessage(),"Messaggio:",0,null);
-        }
-    }
-    
-    
-     // @param db
-    
-    public void closeConnection(DBWrapper db) {
-        db.closeConnection(db);
-    }
-     */
- 
-    /*
-    public static void main(String[] args) {
-        //esempio lettura
- 
-        //apro la connessione
-        DBWrapper db = new DBWrapper();
- 
-        try{
-            //eseguo la query ed ottengo un resultset
-            ResultSet rslt = db.query("SELECT * FROM Utenti");
-            //posso accedere ai metadati
-            ResultSetMetaData rsmd=rslt.getMetaData();
- 
-            int i=rsmd.getColumnCount(); //trova il numero di colonne
- 
-            //leggo tutti i dati e li stampo a video
-            while(rslt.next()){
-                for(int j=1;j<=i;j++){
-                    System.out.print(rslt.getString(j)+" -- ");
-                }
-                System.out.println("");
-            }
- 
-            //chiudo il resultset
-            rslt.close();
-            //chiudo la connessione al DB
-            db.closeConnection();
-        }
-        catch(Exception ez){
-            ez.printStackTrace();
-            System.err.println("errore della query");
-        }
- 
-    }
-    */
+
 }
