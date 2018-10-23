b12560626db6361caab9396937b530ce704cc3de
Giovanroberto Torre <61409177+gbetorre@users.noreply.github.com>
2018-10-23 17:31:05 +0200

    Creato controller per la gestione dei puntamenti GET senza parametri validi

---
+++ src/it/alma/command/HomePageCommand.java
@@ -0,0 +1,196 @@
+/*
+ *   Alma on Line: Applicazione WEB per la visualizzazione 
+ *   delle schede di indagine su popolazione dell'ateneo,
+ *   della gestione dei progetti on line (POL) 
+ *   e della preparazione e del monitoraggio delle informazioni riguardanti 
+ *   l'offerta formativa che hanno ricadute sulla valutazione della didattica 
+ *   (questionari on line - QOL).
+ *   
+ *   Copyright (C) 2018 Giovanroberto Torre<br />
+ *   Alma on Line (aol), Projects on Line (pol), Questionnaire on Line (qol);
+ *   web applications to publish, and manage, students evaluation,
+ *   projects, students and degrees information.
+ *   Copyright (C) renewed 2018 Universita' degli Studi di Verona, 
+ *   all right reserved
+ *
+ *   This program is free software; you can redistribute it and/or modify 
+ *   it under the terms of the GNU General Public License as published by 
+ *   the Free Software Foundation; either version 2 of the License, or 
+ *   (at your option) any later version. 
+ *
+ *   This program is distributed in the hope that it will be useful, 
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ *   GNU General Public License for more details. 
+ *
+ *   You should have received a copy of the GNU General Public License 
+ *   along with this program; if not, write to the Free Software 
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA<br>
+ *   
+ *   Giovanroberto Torre <giovanroberto.torre@univr.it>
+ *   Sistemi Informatici per il Reporting di Ateneo
+ *   Universita' degli Studi di Verona
+ *   Via Dell'Artigliere, 8
+ *   37129 Verona (Italy)
+ */
+
+package it.alma.command;
+
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.oreilly.servlet.ParameterParser;
+
+import it.alma.DBWrapper;
+import it.alma.Main;
+import it.alma.Query;
+import it.alma.bean.ItemBean;
+import it.alma.bean.PersonBean;
+import it.alma.exception.CommandException;
+import it.alma.exception.WebStorageException;
+
+
+/** 
+ * <p><code>HomePageCommand.java</code><br />
+ * Implementa la logica per la gestione dei progetti on line (POL).</p>
+ * 
+ * <p>Created on mercoledì 11 ottobre 2018 15:13</p>
+ * 
+ * @author <a href="mailto:giovanroberto.torre@univr.it">Giovanroberto Torre</a>
+ */
+public class HomePageCommand extends ItemBean implements Command {
+    
+    /**
+     *  Nome di questa classe 
+     *  (utilizzato per contestualizzare i messaggi di errore)
+     */
+    static final String FOR_NAME = "\n" + Logger.getLogger(new Throwable().getStackTrace()[0].getClassName()) + ": ";
+    /**
+     * Log per debug in produzione
+     */
+    protected static Logger LOG = Logger.getLogger(Main.class.getName());
+    /**
+     * Pagina a cui la command reindirizza per mostrare 
+     */
+    private static final String nomeFileElenco = "/jsp/login.jsp"; 
+    /**
+     * Pagina a cui la command fa riferimento per mostrare la Vision del progetto
+     */
+    private static final String nomeFileVision = "/jsp/pcVision.jsp";
+    /**
+     * Pagina a cui la command fa riferimento per mostrare gli Stakeholder del progetto
+     */
+    private static final String nomeFileStakeholder = "/jsp/pcStakeholder.jsp";
+    
+    
+    /** 
+     * Crea una nuova istanza di ValuationCommand 
+     */
+    public HomePageCommand() {
+        ;   // It Doesn't Anything
+    }
+  
+    
+    /** 
+     * <p>Raccoglie i valori dell'oggetto ItemBean
+     * e li passa a questa classe command.</p>
+	 *
+	 * @param voceMenu la VoceMenuBean pari alla Command presente.
+	 * @throws it.alma.exception.CommandException se l'attributo paginaJsp di questa command non e' stato valorizzato.
+     */
+    public void init(ItemBean voceMenu) throws CommandException {
+        this.setId(voceMenu.getId());
+        this.setNome(voceMenu.getNome());
+        this.setLabelWeb(voceMenu.getLabelWeb());
+        this.setNomeClasse(voceMenu.getNomeClasse());
+        this.setPaginaJsp(voceMenu.getPaginaJsp());
+        this.setInformativa(voceMenu.getInformativa());
+        if (this.getPaginaJsp() == null) {
+          String msg = FOR_NAME + "La voce menu' " + this.getNome() + " non ha il campo paginaJsp. Impossibile visualizzare i risultati.\n";
+          throw new CommandException(msg);
+        }
+    }  
+  
+    
+    /**
+     * <p>Gestisce il flusso principale.</p>
+     * <p>Prepara i bean.</p>
+     * <p>Passa nella Request i valori che verranno utilizzati dall'applicazione.</p>
+     */
+    public void execute(HttpServletRequest req) 
+                 throws CommandException {
+        // Databound
+        DBWrapper db = null;
+        // Parser per la gestione assistita dei parametri di input
+        ParameterParser parser = new ParameterParser(req);
+        // Utente loggato
+        PersonBean user = null;
+        /* ******************************************************************** *
+         *      Instanzia nuova classe WebStorage per il recupero dei dati      *
+         * ******************************************************************** */
+        try {
+            db = new DBWrapper();
+        } catch (WebStorageException wse) {
+            throw new CommandException(FOR_NAME + "Non e\' disponibile un collegamento al database\n." + wse.getMessage(), wse);
+        }
+        /* ******************************************************************** *
+         *                         Recupera la Sessione                         *
+         * ******************************************************************** */
+        try {
+            // Recupera la sessione creata e valorizzata per riferimento nella req dal metodo authenticate
+            HttpSession ses = req.getSession(Query.IF_EXISTS_DONOT_CREATE_NEW);
+            user = (PersonBean) ses.getAttribute("usr");
+        } catch (IllegalStateException ise) {
+            String msg = FOR_NAME + "Impossibile redirigere l'output. Verificare se la risposta e\' stata gia\' committata.\n";
+            LOG.severe(msg);
+            throw new CommandException(msg + ise.getMessage(), ise);
+        } catch (ClassCastException cce) {
+            String msg = FOR_NAME + ": Si e\' verificato un problema in una conversione di tipo.\n";
+            LOG.severe(msg);
+            throw new CommandException(msg + cce.getMessage(), cce);
+        } catch (NullPointerException npe) {
+            String msg = FOR_NAME + "Si e\' verificato un problema di puntamento a null, probabilmente nel tentativo di recuperare l\'utente.\n";
+            LOG.severe(msg);
+            throw new CommandException("Attenzione: controllare di essere autenticati nell\'applicazione!\n" + npe.getMessage(), npe);
+        } catch (Exception e) {
+            String msg = FOR_NAME + "Si e\' verificato un problema.\n";
+            LOG.severe(msg);
+            throw new CommandException(msg + e.getMessage(), e);
+        }
+        /* ******************************************************************** *
+         *                          Recupera i parametri                        *
+         * ******************************************************************** */
+        // Recupera o inizializza 'id progetto'
+        int idPrj = parser.getIntParameter("id", -1); 
+        // Recupera o inizializza 'tipo corsi di studio'
+        String tcs = parser.getStringParameter("tcs", "-");
+        // Recupera o inizializza 'tipo pagina'   
+        String part = parser.getStringParameter("p", "-");
+        // Dichiara la pagina a cui reindirizzare
+        String fileJspT = null;
+        // Decide il valore della pagina
+        if (part.equalsIgnoreCase("pcv")) {
+            fileJspT = nomeFileVision;
+        } else if (part.equalsIgnoreCase("pcs")) {
+            fileJspT = nomeFileStakeholder;
+        } else {
+            fileJspT = nomeFileElenco;
+        }
+        // Recupera il tipo di documento
+        //String tipo = parser.getStringParameter("tipo", "-");
+        // Ricerca il bean della ex-facolta', o del dipartimento, etc.
+        //int idScuDott = parser.getIntParameter("scuoladott", -1);
+        
+        // Imposta il testo del Titolo da visualizzare prima dell'elenco
+        req.setAttribute("titoloE", "Project Charter");
+        // Salva nella request: Titolo pagina (da mostrare nell'HTML)
+        req.setAttribute("tP", req.getAttribute("titoloE"));         
+        // Imposta la Pagina JSP di forwarding
+        req.setAttribute("fileJsp", nomeFileElenco);
+        // Salva nella request: utente
+        //req.setAttribute("user", user);
+    }
+    
+}
